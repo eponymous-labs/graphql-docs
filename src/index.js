1c86fc0c2c883291fb2a4a5597d7526928bb4832
Kevin Kwok <antimatter15@gmail.com>
2019-06-25 03:31:36 -0400

    changing public facing api, upgrading deps

--- src/index.js
+++ src/index.js
@@ -5,34 +5,13 @@ import React from 'react';
 import { SchemaDocsView } from './components/SchemaDocsView';
 import { Schema } from './model';
 
-import introspectionQuery from './introspectionQuery.txt';
-
-export type Fetcher = (query: string) => Promise<any>;
 
 export class GraphQLDocs extends React.Component {
-    props: {
-        fetcher: Fetcher;
-    };
-
-    state: {
-        schema: Schema;
-    };
-
-    componentWillMount() {
-        const promise = this.props.fetcher(introspectionQuery);
-
-        promise.then(json => {
-            this.setState({
-                schema: new Schema(json.data),
-            });
-        });
+    shouldComponentUpdate(nextProps){
+        return this.props.schema !== nextProps.schema
     }
 
     render() {
-        if (this.state && this.state.schema) {
-            return <SchemaDocsView schema={this.state.schema} />;
-        } else {
-            return <div />;
-        }
+        return <SchemaDocsView schema={new Schema(this.props.schema)} />;
     }
 }
